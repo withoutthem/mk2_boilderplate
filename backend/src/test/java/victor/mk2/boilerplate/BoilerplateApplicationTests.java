3ac8f3468abc5c3bb5cadc76af15fe7a26c37cda
Victor <withoutthem@naver.com>
2024-02-14 13:30:11 +0900

    init

---
+++ backend/src/test/java/victor/mk2/boilerplate/BoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.mk2.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
