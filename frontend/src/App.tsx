4ff185d470d74d304651b6e5804efc888f673b78
Victor <withoutthem@naver.com>
2024-02-14 21:26:56 +0900

    개빡치네 리셋

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">asdfsadfasfdsadfsdfasdf sdf asdf asdfasdfsdaf</div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
